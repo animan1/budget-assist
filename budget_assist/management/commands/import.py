69721e5c12dc36bd513dc5504eadef0769c9cab7
Daniel Boeve <dboeve@hearsaycorp.com>
2016-10-19 22:32:20 -0700

    Read rows into a data structure

--- budget_assist/management/commands/import.py
+++ budget_assist/management/commands/import.py
@@ -1,4 +1,18 @@
+import csv
+from collections import OrderedDict
 from django.core.management.base import BaseCommand
+
+
+class Transaction(object):
+
+    def __init__(self, data):
+        self.data = data
+
+    @classmethod
+    def from_csv_row(cls, header, row):
+        data = OrderedDict(zip(header, row))
+        return cls(data)
+
 
 class Command(BaseCommand):
     help = 'Import a transactions CSV from Mint'
@@ -7,4 +21,9 @@ class Command(BaseCommand):
         parser.add_argument('transactions_filename', type=str)
 
     def handle(self, *args, **options):
-        print 'Called the import command with %s' % options['transactions_filename']
+        filename = options['transactions_filename']
+        with open(filename) as transactions_file:
+            reader = csv.reader(transactions_file)
+            header = reader.next()
+            transactions = [Transaction.from_csv_row(header, row) for row in reader]
+        print transactions[0].data
