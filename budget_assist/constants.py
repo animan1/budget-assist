7986f1016f3bf24c511730bd8d1a6643bebf54d7
Daniel Boeve <dboeve@hearsaycorp.com>
2016-11-07 22:07:50 -0800

    Adds transaction type enum

--- budget_assist/constants.py
+++ budget_assist/constants.py
@@ -1,5 +1,6 @@
 from richenum import RichEnum, RichEnumValue
 
+
 class LabelValue(RichEnumValue):
 
     def __init__(self, *args):
@@ -11,3 +12,17 @@ class LabelValue(RichEnumValue):
 class Label(RichEnum):
 
     WRONG_ACCOUNT = LabelValue('Wrong account used', 'Wrong Account Used', '')
+
+class TransactionTypeValue(RichEnumValue):
+
+    def __init__(self, inverse_canonical_name, *args):
+        super(TransactionTypeValue, self).__init__(*args)
+        self.inverse_canonical_name = inverse_canonical_name
+
+    def inverse(self):
+        return TransactionType.from_canonical(self.inverse_canonical_name)
+
+class TransactionType(RichEnum):
+
+    CREDIT = TransactionTypeValue('debit', 'credit', 'credit', '')
+    DEDIT = TransactionTypeValue('credit', 'debit', 'dedit', '')
